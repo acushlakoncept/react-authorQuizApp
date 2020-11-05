7a60f8c405e42f37136e6932136e94d6c509ed44
Uduak Essien <acushla4real@gmail.com>
2020-11-05 03:24:13 +0100

    add hero, turn and continue function

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,10 +1,36 @@
+import React, { Component } from 'react'
 import './App.css';
 import './bootstrap.min.css'
 
+function Hero() {
+  return (
+    <div className="row">
+      <div className="jumbotron col-10 offset-1">
+        <h1>Author Quiz</h1>
+        <p>Select the book written by the author shown</p>
+      </div>
+    </div>
+  )
+}
+
+function Turn() {
+  return (
+    <div></div>
+  )
+}
+
+function Continue() {
+  return (
+    <div></div>
+  )
+}
+
 function AuthorQuiz() {
   return (
-    <div>
-       Author Quiz
+    <div className="container-fluid">
+       <Hero />
+       <Turn />
+       <Continue />
     </div>
   );
 }

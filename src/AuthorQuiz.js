fcba565649d27d44869bb0f7e6052992d03f07d1
Uduak Essien <acushla4real@gmail.com>
2020-11-11 13:47:37 +0100

    add proptypes validation

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types'
+
 import "./App.css";
 import "./bootstrap.min.css";
 
@@ -43,6 +45,18 @@ function Turn({author, books, highlight, onAnswerSelected}) {
     </div>
   );
 }
+
+Turn.prototype = {
+  author: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    imageSource: PropTypes.string.isRequired,
+    books: PropTypes.arrayOf(PropTypes.string).isRequired
+  }),
+  books: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onAnswerSelected: PropTypes.func.isRequired,
+  highlight: PropTypes.string.isRequired
+};
 
 function Continue() {
   return <div></div>;

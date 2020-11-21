cc0602d8db5734ce789112924e5b46557a1e28c7
Uduak Essien <acushla4real@gmail.com>
2020-11-21 11:47:15 +0100

    refactor AddAuthorForm, create AuthoForm class with states

--- src/AddAuthorForm.js
+++ src/AddAuthorForm.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React from "react";
 import "./AddAuthorForm.css";
 
-export default function AddAuthorForm({match}) {
-    return <div className="AddAuthorForm">
-      <h1>Add Author</h1>
-      <form>
-          <div className="AddAuthorForm__input">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" />
-          </div>
-      </form>
-    </div>
+class AuthorForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      imageUrl: ""
+    };
   }
+
+  onFieldChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <div className="AddAuthorForm__input">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" value={this.state.name} />
+        </div>
+        <div className="AddAuthorForm__input">
+          <label htmlFor="imageUrl">Image URL</label>
+          <input type="text" name="imageUrl" value={this.state.imageUrl} />
+        </div>
+      </form>
+    );
+  }
+}
+
+export default function AddAuthorForm({ match }) {
+  return (
+    <div className="AddAuthorForm">
+      <h1>Add Author</h1>
+      <AuthorForm />
+    </div>
+  );
+}

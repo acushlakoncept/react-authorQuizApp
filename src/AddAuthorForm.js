69d1d7a61713278a6a7eab84ac1374209b7d1eac
Uduak Essien <acushla4real@gmail.com>
2020-11-21 11:40:51 +0100

    add AddAuthorForm css

--- src/AddAuthorForm.js
+++ src/AddAuthorForm.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import "./AddAuthorForm.css";
 
 export default function AddAuthorForm({match}) {
-    return <div>
+    return <div className="AddAuthorForm">
       <h1>Add Author</h1>
       <form>
-          <div>
+          <div className="AddAuthorForm__input">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" />
           </div>
